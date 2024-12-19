128fa16874975928b2c330f60fef354fda6ec144
Lukas Norrman <lukas.norrman@outlook.com>
2024-12-19 11:04:12 +0100

    hej

--- ToDoProject/src/main/java/com/example/ToDoProject/ToDoProjectApplication.java
+++ ToDoProject/src/main/java/com/example/ToDoProject/ToDoProjectApplication.java
@@ -8,6 +8,7 @@ public class ToDoProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ToDoProjectApplication.class, args);
+		System.out.println("Test2");
 	}
 
 }
