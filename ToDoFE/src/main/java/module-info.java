bbc235c9b7409fe74daae84ac77be87c8eab1b2d
Lukas Norrman <lukas.norrman@outlook.com>
2024-12-19 14:02:21 +0100

    Implemented methods in Controller

--- ToDoFE/src/main/java/module-info.java
+++ ToDoFE/src/main/java/module-info.java
@@ -3,6 +3,7 @@ module org.example.todofe {
     requires javafx.fxml;
 
     requires com.dlsc.formsfx;
+    requires java.desktop;
 
     opens org.example.todofe to javafx.fxml;
     exports org.example.todofe;
--- ToDoFE/src/main/java/org/example/todofe/HelloController.java
+++ ToDoFE/src/main/java/org/example/todofe/HelloController.java
@@ -2,13 +2,63 @@ package org.example.todofe;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+
+import java.awt.event.ActionEvent;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField input_Description;
+
+    @FXML
+    private TableColumn<?, ?> input_TableDesc;
+
+    @FXML
+    private TableColumn<?, ?> input_TableID;
+
+    @FXML
+    private TableColumn<?, ?> input_TableTaskName;
+
+    @FXML
+    private TextField input_Task;
+
+    @FXML
+    private TextField input_TaskID;
+
+    @FXML
+    private TextField input_TaskID2;
+
+    @FXML
+    void addTask(ActionEvent event) {
+
+        try {
+            String taskName = input_Task.getText();
+            String description = input_Description.getText();
+            int taskId = Integer.parseInt(input_TaskID.getText());
+
+            if (taskName.isEmpty() || description.isEmpty()) {
+                input_Description.setText("Please fill in both the task name and description. ");
+            }
+            // Ska lägga till mer Kod här
+        } catch (Exception e) {
+            input_TaskID.setText("Error " + e.getMessage());
+        }
+
     }
+
+    @FXML
+    void deleteTask(ActionEvent event) {
+
+
+    }
+
+    @FXML
+    void editTask(ActionEvent event) {
+
+    }
+
 }
+
