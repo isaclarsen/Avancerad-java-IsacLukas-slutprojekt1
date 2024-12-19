666ec51ed8f66f331c357566932765496bf0dc96
Isac Larsen <isaclarsen22@gmail.com>
2024-12-19 13:57:47 +0100

    ToDo Class

---
+++ ToDoProject/src/main/java/com/example/ToDoProject/Controllers/ToDoController.java
@@ -0,0 +1,13 @@
+package com.example.ToDoProject.Controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/api/tasks")
+
+public class ToDoController {
+    //Lägg in datastrukut (Arraylist tror jag)
+}
