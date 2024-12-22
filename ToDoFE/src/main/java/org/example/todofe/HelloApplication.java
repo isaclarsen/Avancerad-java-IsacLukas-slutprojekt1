9243ff7b16a8f37dcae9e99e0824761c3e9ab8a3
Isac Larsen <isaclarsen22@gmail.com>
2024-12-22 13:40:54 +0100

    Changed window size and title

--- ToDoFE/src/main/java/org/example/todofe/HelloApplication.java
+++ ToDoFE/src/main/java/org/example/todofe/HelloApplication.java
@@ -14,8 +14,8 @@ public class HelloApplication extends Application {
 
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
 
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 550, 700);
+        stage.setTitle("ToDo!");
         stage.setScene(scene);
         stage.show();
     }
